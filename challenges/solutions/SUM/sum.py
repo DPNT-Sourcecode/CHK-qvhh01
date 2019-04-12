59b39b5927e8869d8cf076376c10ae4db41b46aa
sbx_user1051 <sbx_user1051@169.254.108.137>
2019-04-12 16:29:35 +0000

    Fri Apr 12 16:29:35 UTC 2019

--- challenges/solutions/SUM/sum.py
+++
@@ -1,6 +0,0 @@
-
-class sum_solution:
-    @staticmethod
-    def compute(a, b):
-        ''' Sum of 2 interger '''
-        return a+b
---
+++ challenges/solutions/SUM/sum_solution.py
@@ -0,0 +1,5 @@
+
+
+def compute(a, b):
+    ''' Sum of 2 interger '''
+    return a+b
