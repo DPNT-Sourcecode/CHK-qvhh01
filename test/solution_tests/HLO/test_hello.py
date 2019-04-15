202e4329e0a5d757bb6dde5aea3a5cc91c3e6ac5
sbx_user1051 <sbx_user1051@169.254.127.141>
2019-04-15 18:05:52 +0000

    Mon Apr 15 18:05:52 UTC 2019

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,7 +3,6 @@ from lib.solutions.HLO import hello_solution
 
 class TestHello():
     def test_hello(self):
-        name = 'Yaya'
-        assert hello_solution.hello(name) == 'Hello, World!'
+        name = 'John'
+        assert hello_solution.hello(name) == 'Hello, {}!'.format(name)
     
-
