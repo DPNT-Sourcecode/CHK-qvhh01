dc64ca146662ce7c0ef64b62215c3bd2cd308424
sbx_user1051 <sbx_user1051@169.254.241.33>
2019-04-16 00:54:47 +0000

    Tue Apr 16 00:54:47 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,85 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+# def checkout(skus):
+#     raise NotImplementedError()
+import json
+class Stock():
+    SKUS_JSON = "[{ \"product_name\" : \"A\", \"price\" : 50, \"discount_purchase\" : 3, \"discount_receive\" : 130},\
+                    { \"product_name\" : \"B\", \"price\" : 30, \"discount_purchase\" : 2, \"discount_receive\" : 45},\
+                    { \"product_name\" : \"C\", \"price\" : 20}, \
+                    { \"product_name\" : \"D\", \"price\" : 15}]"
+    
+
+
+class Skus(object):
+    def __init__(self, product_name, price, discount_purchase=None, discount_receive=None):
+        self.product_name = product_name
+        self.price = price
+        self.discount_purchase = discount_purchase
+        self.discount_receive = discount_receive
+        self.number_of_items = 1
+
+    def get_product_name(self):
+        return self.product_name
+
+    def get_price(self):
+        if self.discount_purchase:
+            if self.number_of_items >= self.discount_purchase:
+                if (self.number_of_items % self.discount_purchase) == 0:
+                    return (self.discount_receive * (self.number_of_items / self.discount_purchase))
+                else:
+                     return (self.discount_receive * (self.number_of_items // self.discount_purchase) + ((self.number_of_items % self.discount_purchase) * self.price))
+
+        return self.number_of_items * self.price
+
+    def get_number_of_items(self):
+        return self.number_of_items
+
+    def get_discount_purchase(self):
+        return self.discount_purchase
+
+    def get_discount_receive(self):
+        return self.discount_receive
+    
+    def increment_number_of_items(self):
+        self.number_of_items += 1
+
+class Basket():
+
+    def __init__(self):
+        self.items_list = list()
+
+    def add_item(self, item):
+        found = False
+        index = 0
+        for el in self.items_list:
+            index += 1
+            if el.get_product_name() == item.get_product_name():
+                found = True
+                break
+
+        if found:
+            self.items_list[index - 1].increment_number_of_items()
+        else:
+            self.items_list.append(item)
+    
+    def get_items(self):
+        return self.items_list
+    
+    def get_total(self):
+        return sum(map(lambda item : item.get_price(), self.items_list))
+    
+    def checkout(self, skus_string):
+        stock = []
+        data = json.loads(Stock.SKUS_JSON)
+        for dt in data:
+            skus = Skus(**dt)
+            stock.append(skus)
+        
+        for skus_name in skus_string:
+            skus_object = list(filter(lambda item : item.product_name == skus_name, stock))[0]
+            self.add_item(skus_object)
+
+        return self.get_total()
+        
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,6 +1,116 @@
 from lib.solutions.CHK import checkout_solution
 
+class TestSkus():
+    def test_skus_should_created(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=10.0)
+        assert skus_a.get_product_name() == 'A'
+        assert skus_a.get_price() == 10.0
+        assert skus_a.get_number_of_items() == 1
+        assert skus_a.get_discount_purchase() == None
+        assert skus_a.get_discount_receive() == None
+    
+    def test_skus_should_created_with_discount_info(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50, discount_purchase=3, discount_receive=130)
+        assert skus_a.get_product_name() == 'A'
+        assert skus_a.get_price() == 50
+        assert skus_a.get_number_of_items() == 1
+        assert skus_a.get_discount_purchase() == 3
+        assert skus_a.get_discount_receive() == 130
 
-class TestSum():
-    def test_should_return_total(self):
-        assert checkout_solution.compute(1, 2) == 3
+class TestBasket():
+    
+    def test_basket_items(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50, discount_purchase=3, discount_receive=130)
+        basket = checkout_solution.Basket()
+        assert len(basket.get_items()) == 0
+        basket.add_item(skus_a)
+        assert len(basket.get_items()) == 1
+
+    def test_when_added_the_same_skus_in_basket(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50, discount_purchase=3, discount_receive=130)
+        skus_b = checkout_solution.Skus(product_name='B', price=30, discount_purchase=2, discount_receive=45)
+        skus_c = checkout_solution.Skus(product_name='C', price=20)
+        basket = checkout_solution.Basket()
+        basket.add_item(skus_a)
+        basket.add_item(skus_b)
+        basket.add_item(skus_c)
+        items_list = basket.get_items()
+        assert len(items_list) == 3
+        assert items_list[1].get_number_of_items() == 1
+        basket.add_item(skus_b)
+        items_list = basket.get_items()
+        assert len(items_list) == 3
+        assert items_list[1].get_number_of_items() == 2
+
+    def test_checkout_without_discount(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50)
+        skus_b = checkout_solution.Skus(product_name='B', price=30)
+        skus_c = checkout_solution.Skus(product_name='C', price=20)
+        basket = checkout_solution.Basket()
+        basket.add_item(skus_a)
+        basket.add_item(skus_b)
+        basket.add_item(skus_c)
+        assert basket.get_total() == 100
+    
+    def test_checkout_with_discount_not_applied(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50, discount_purchase=3, discount_receive=130)
+        skus_b = checkout_solution.Skus(product_name='B', price=30)
+        skus_c = checkout_solution.Skus(product_name='C', price=20)
+        basket = checkout_solution.Basket()
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_b)
+        basket.add_item(skus_c)
+        assert basket.get_total() == 150
+
+    def test_checkout_with_discount_applied(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50, discount_purchase=3, discount_receive=130)
+        skus_b = checkout_solution.Skus(product_name='B', price=30)
+        skus_c = checkout_solution.Skus(product_name='C', price=20)
+        basket = checkout_solution.Basket()
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_b)
+        basket.add_item(skus_c)
+        assert basket.get_total() == 180
+    
+    def test_checkout_with_discount_applied_but_not_on_all_items(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50, discount_purchase=3, discount_receive=130)
+        skus_b = checkout_solution.Skus(product_name='B', price=30)
+        skus_c = checkout_solution.Skus(product_name='C', price=20)
+        basket = checkout_solution.Basket()
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_b)
+        basket.add_item(skus_c)
+        assert basket.get_total() == 230
+    
+    def test_checkout_with_discount_applied_but_not_on_all_items_(self):
+        skus_a = checkout_solution.Skus(product_name='A', price=50, discount_purchase=2, discount_receive=80)
+        skus_b = checkout_solution.Skus(product_name='B', price=30)
+        skus_c = checkout_solution.Skus(product_name='C', price=20)
+        basket = checkout_solution.Basket()
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_a)
+        basket.add_item(skus_b)
+        basket.add_item(skus_c)
+        assert basket.get_total() == 260
+
+    def test_checkout_from_string(self):
+        basket = checkout_solution.Basket()
+        assert basket.checkout('ABCD') == 115
+    
+    def test_checkout_from_string_discount(self):
+        basket = checkout_solution.Basket()
+        assert basket.checkout('ABCADA') == 195
+        
+            
+
+
+
